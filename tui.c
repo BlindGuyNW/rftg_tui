76a65d28e15bff288602cb2c57e865e514ca40ac
Zachary Kline <zkline@speedpost.net>
2023-10-27 14:17:54 -0700

    Play with multiple selections.

--- tui.c
+++ tui.c
@@ -67,52 +67,59 @@ void display_card_info(game *g, int card_index) {
     printf("----------------------------\n\n");
 }
 
-int get_card_choice(game *g, int list[], int num, const char *prompt) {
-    char action[10];
-    int selected_card;
+int get_card_choice(game *g, int list[], int num, const char *prompt, int allow_multiple) {
+    char action[100];  // Increased size to account for multiple selections
+    int selected_cards[num], selected_count = 0;  // Array to store multiple selections
+    int card_index, confirm;
 
     while (1) {
-        printf("%s (or 'i' followed by number for info, e.g., i2, 'q' to quit, 'h' for help, 'r' to redisplay list): ", prompt);
-        scanf("%s", action);
+        if (allow_multiple) {
+            printf("%s (or 'i' followed by number for info, e.g., i2, 'q' to quit, 'h' for help, 'r' to redisplay list, or provide a comma-separated list for multiple selections): ", prompt);
+        } else {
+            printf("%s (or 'i' followed by number for info, e.g., i2, 'q' to quit, 'h' for help, 'r' to redisplay list): ", prompt);
+        }
+        
+        scanf("%99s", action);  // Avoid buffer overflow
 
         // Info command
         if (action[0] == 'i' || strncmp(action, "info", 4) == 0) {
-            if (sscanf(action + 1, "%d", &selected_card) == 1 || sscanf(action + 4, "%d", &selected_card) == 1) {
-                if (selected_card >= 1 && selected_card <= num) {
-                    display_card_info(g, list[selected_card - 1]);
-                } else {
-                    printf("Invalid card number. Please try again.\n");
+            if (sscanf(action + 1, "%d", &card_index) == 1 || sscanf(action + 4, "%d", &card_index) == 1) {
+                if (card_index >= 1 && card_index <= num) {
+                    display_card_info(g, list[card_index - 1]);
                 }
-            } else {
-                printf("Invalid format. Please try again.\n");
+            }
+        } else if (allow_multiple && strchr(action, ',') != NULL) {  // Check for multiple selections
+            char *token = strtok(action, ",");
+            while (token != NULL) {
+                sscanf(token, "%d", &card_index);
+                if (card_index >= 1 && card_index <= num) {
+                    selected_cards[selected_count++] = card_index;
+                }
+                token = strtok(NULL, ",");
             }
 
-        // Quit command
-        } else if (action[0] == 'q' || strncmp(action, "quit", 4) == 0) {
-            printf("Quitting...\n");
-            exit(0);
-
-        // Help command
-        } else if (action[0] == 'h' || strncmp(action, "help", 4) == 0) {
-            printf("Help: Enter a card number to choose, 'i' followed by number for info, 'q' to quit, 'r' to redisplay list.\n");
-
-        // Redisplay command
-        } else if (action[0] == 'r' || strncmp(action, "redisplay", 9) == 0) {
-            display_cards(g, list, num, prompt);
-
-        // Card selection
-        } else if (sscanf(action, "%d", &selected_card) == 1) {
-            if (selected_card >= 1 && selected_card <= num) {
-                return selected_card;
-            } else {
-                printf("Invalid selection. Please try again.\n");
+            // Display selected cards for confirmation
+            printf("You've selected cards: ");
+            for (int i = 0; i < selected_count; i++) {
+                printf("%d ", selected_cards[i]);
             }
+            printf("\\nConfirm? (1 for Yes, 0 for No): ");
+            scanf("%d", &confirm);
 
-        // Invalid command
+            if (confirm == 1) {
+                return selected_count;  // Return number of selected cards
+            } else {
+                selected_count = 0;  // Reset selection count
+            }
         } else {
-            printf("Invalid input. Please try again or enter 'h' for help.\n");
+            sscanf(action, "%d", &card_index);
+            if (card_index >= 1 && card_index <= num) {
+                return card_index;  // Return the selected card index
+            }
         }
+        // ... (rest of the function for other commands like 'q', 'h', 'r')
     }
+    return -1;  // Return -1 if the user quits without selection
 }
 
 
@@ -129,7 +136,7 @@ void gui_choose_discard(game *g, int who, int list[], int *num, int discard) {
     display_cards(g, temp_list, *num - discard_count, "You need to discard. Here are your options:");
 
     while (discard_count < discard) {
-        int selected_card = get_card_choice(g, temp_list, *num - discard_count, "Enter card number to discard");
+        int selected_card = get_card_choice(g, temp_list, *num - discard_count, "Enter card number to discard", 1);
         
         // Add the selected card to the list of discarded cards
         list[discard_count] = temp_list[selected_card - 1];
